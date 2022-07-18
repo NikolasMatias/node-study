29f9dcebdc38548fe8dfa3781b05ebcf63d7b156
Nikolas Matias <nikolas.matias500@gmail.com>
2022-07-18 07:00:29 -0300

    Test of URL

--- hello-world.js
+++ hello-world.js
@@ -5,8 +5,19 @@ const port = 3000;
 
 const server = http.createServer(((req, res) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World\n');
+    // console.log(res.socket);
+    console.log(req.url);
+    console.log(req.method);
+
+    switch (req.url) {
+        case '/teste':
+            res.setHeader('Content-Type', 'text/plain');
+            res.end('O teste de rotas deu certo');
+            break;
+        default:
+            res.setHeader('Content-Type', 'text/plain');
+            res.end('Hello, World\n');
+    }
 }));
 
 server.listen(port, hostname, () => {
